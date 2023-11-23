681be227e1499d1b6b80bd5e2cbdbec583b80409
b0cchi <b0cchi.vv@gmail.com>
2023-11-23 22:32:11 +0900

    add source

---
+++ workspace/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { interval } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+})
+export class AppComponent implements OnInit {
+  title = 'CurrentTime';
+  today = new Date();
+  currentTime = signal(this.today);
+  constructor() {}
+  ngOnInit(): void {
+    this.start();
+  }
+  start(): void {
+    interval(200).subscribe((x) => {
+      this.currentTime.update((v) => (v = new Date()));
+    });
+  }
+}
