f7fc24682f07ac9fca86337c4a7f141d28414e44
b0cchi <b0cchi.vv@gmail.com>
2023-12-07 23:28:26 +0900

    曜日も追加

--- workspace/src/app/app.component.ts
+++ workspace/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, signal } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { interval } from 'rxjs';
 
@@ -7,18 +7,46 @@ import { interval } from 'rxjs';
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
+  providers: [DatePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
   title = 'CurrentTime';
   currentTime = signal(new Date());
-  constructor() {}
+  weekOfDay = signal('');
+  constructor(private datePipe: DatePipe) {}
   ngOnInit(): void {
     this.start();
   }
   start(): void {
     interval(200).subscribe((x) => {
+      const week = this.datePipe.transform(new Date(), 'E');
+      let weekOfDayJa = '';
+      switch (week) {
+        case 'Sun':
+          weekOfDayJa = '日';
+          break;
+        case 'Mon':
+          weekOfDayJa = '月';
+          break;
+        case 'Tue':
+          weekOfDayJa = '火';
+          break;
+        case 'Wed':
+          weekOfDayJa = '水';
+          break;
+        case 'Thu':
+          weekOfDayJa = '木';
+          break;
+        case 'Fri':
+          weekOfDayJa = '金';
+          break;
+        case 'Sat':
+          weekOfDayJa = '土';
+          break;
+      }
+      this.weekOfDay.update((v) => (v = weekOfDayJa));
       this.currentTime.update((v) => (v = new Date()));
     });
   }
